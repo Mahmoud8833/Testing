d0b85b14c1af5f58305e1aa5935f4e0d76a2ccc7
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-08-18 20:12:18 +0200

    adding an index page

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
+
+//-----------------------------------------
+// A function for finding avarage num
+// function average(...nums) {
+//   let total = 0;
+//   if (nums.length === 0) {
+//     total = 0;
+//     return total;
+//   } else {
+//     for (const num of nums) {
+//       total += parseInt(num);
+//     }
+//     return total / nums.length;
+//   }
+// }
+
+// console.log(average());
+// console.log(average(2, 6));
+// console.log(average(2, 3, 3, 5, 7, 10));
+// console.log(average(7, 1432, 12, 13, 100));
+
 //-----------------------------------------
 //Sum any number of arguments using the for of loop on a rest array
 
