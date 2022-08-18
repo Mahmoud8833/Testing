f6cebde114eb649434f7dbbe19de76c3b6fcaa4d
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-08-18 23:24:51 +0200

    Updat qqq.js

--- qqq.js
+++ qqq.js
@@ -1,1 +1,24 @@
-console.log("Hello JavaScript");
+"use strict";
+
+// const COLOR_ORANGE = "#FF7F00";
+// let color = COLOR_ORANGE;
+// alert(color); // #FF7F00
+// const myName = 'Mahmoud'
+
+// let name = "Mahmoud",
+//   age = 35,
+//   gender = "Male";
+
+// let message = "Hello!";
+// console.log(message);
+
+/**
+ * an extended comment
+ *
+ *
+ */
+
+//____________________
+
+//First alert
+//console.log("Hello JavaScript!");
