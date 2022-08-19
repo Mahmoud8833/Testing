a771fcbb609c3131e3887777a0febfb08866bc7a
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-08-20 00:07:16 +0200

    Update qqq.js

--- qqq.js
+++ qqq.js
@@ -1,5 +1,36 @@
 "use strict";
 
+/**
+ * object
+ */
+
+/**
+ * undefined
+ */
+
+/**
+ * null => nothing
+ */
+
+
+/**
+ * Boolean
+ * => true
+ * => false
+ */
+
+/** string
+*let str1 = "mm"; // double quotes
+*let str2 = 'mm'; // single
+*let str3 = `mm`; // backticks
+*let str = `anything ${str3}`
+*/
+
+/**
+ * Numbers
+ * JavaScript => 64bit Storage
+ *  */ 
+
 // const COLOR_ORANGE = "#FF7F00";
 // let color = COLOR_ORANGE;
 // alert(color); // #FF7F00
