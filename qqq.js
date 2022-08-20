68208a7257f31eacbb9a4ca9334bd6f6c24c181e
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-08-20 03:09:42 +0200

    update qqq.js

--- qqq.js
+++ qqq.js
@@ -1,5 +1,26 @@
 "use strict";
 
+
+// Addition +,
+// Subtraction -,
+// Multiplication *,
+// Division /,
+// Remainder %,
+// Exponentiation **.
+
+// let result = confirm('are you 18+ years old?')
+// alert(result)
+// let fName = prompt('what is yor first name?', 'Mahmoud')
+// let lName = prompt("what is yor last name?", 'Ali');
+// alert(`Your full name is: ${fName+' '+lName}`)
+// alert('Hello!')
+
+/**
+ * typeof operator x
+ * console.log(typeof alert);
+ *
+ */
+
 /**
  * object
  */
@@ -12,7 +33,6 @@
  * null => nothing
  */
 
-
 /**
  * Boolean
  * => true
@@ -20,16 +40,17 @@
  */
 
 /** string
-*let str1 = "mm"; // double quotes
-*let str2 = 'mm'; // single
-*let str3 = `mm`; // backticks
-*let str = `anything ${str3}`
-*/
+ *let str1 = "mm"; // double quotes
+ *let str2 = 'mm'; // single
+ *let str3 = `mm`; // backticks
+ *let str = `anything ${str3}`
+ */
 
 /**
  * Numbers
+ * BigInt
  * JavaScript => 64bit Storage
- *  */ 
+ *  */
 
 // const COLOR_ORANGE = "#FF7F00";
 // let color = COLOR_ORANGE;
